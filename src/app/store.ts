6e85b33cdd6d3d37888fc5c2013252c7a8b780b8
wunna11 <wana11391@gmail.com>
2024-02-25 00:05:02 +0630

    Fix delete counter slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import coffeeReducer from '../features/coffeeSlice';
-import counterReducer from '../features/counterSlice';
 import cartReducer from '../features/cartSlice';
 import orderReducer from '../features/orderSlice';
 
@@ -8,7 +7,6 @@ import orderReducer from '../features/orderSlice';
 export const store = configureStore({
     reducer: {
         coffee: coffeeReducer,
-        counter: counterReducer,
         cart: cartReducer,
         order: orderReducer,
     },
